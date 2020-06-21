205ceec810f5fd2b4b137187e4d98ded5f5143bf
Alessander <alessander.logistica@gmail.com>
2020-06-21 00:03:26 -0300

    add custom methods

--- src/main/java/com/example/app/domain/repository/Pedidos.java
+++ src/main/java/com/example/app/domain/repository/Pedidos.java
@@ -1,9 +1,19 @@
 package com.example.app.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.app.domain.entity.Cliente;
 import com.example.app.domain.entity.Pedido;
 
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
-
+	
+	List<Pedido> findByCliente(Cliente cliente);
+	
+	@Query(" select p from Pedido p left join fetch p.itens where p.id = :id ")
+	Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
 }
