830eaad35c521014b84bf2bc76e7dd1a3611366c
Alessander <alessander.logistica@gmail.com>
2020-06-20 23:58:34 -0300

    create the interface Produtos

---
+++ src/main/java/com/example/app/domain/repository/Produtos.java
@@ -0,0 +1,7 @@
+package com.example.app.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produtos extends JpaRepository<Produtos, Integer> {
+
+}
