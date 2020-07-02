0dc1ef01a9f0d889f52aad20a726b866b8e61241
Alessander <alessander.logistica@gmail.com>
2020-07-01 22:40:52 -0300

    create the controller class named UsuarioController

---
+++ src/main/java/com/example/app/rest/controller/UsuarioController.java
@@ -0,0 +1,57 @@
+package com.example.app.rest.controller;
+
+import javax.validation.Valid;
+
+import org.jboss.jandex.TypeTarget.Usage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.app.domain.entity.Usuario;
+import com.example.app.exception.SenhaInvalidaException;
+import com.example.app.rest.dto.CredenciaisDTO;
+import com.example.app.rest.dto.TokenDTO;
+import com.example.app.security.jwt.JwtService;
+import com.example.app.service.impl.UsuarioServiceImpl;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioServiceImpl usuarioService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	@Autowired
+	private JwtService jwtService;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario salvar(@RequestBody @Valid Usuario usuario) {
+		String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaCriptografada);
+		return usuarioService.salvar(usuario);
+	}
+	
+	@PostMapping("/auth")
+	public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais) {
+		try {
+			Usuario usuario = new Usuario();
+			usuario.setLogin(credenciais.getLogin());
+			usuario.setSenha(credenciais.getSenha());
+			UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+			String token = jwtService.gerarToken(usuario);
+			return new TokenDTO(usuario.getLogin(), token);
+		} catch (UsernameNotFoundException | SenhaInvalidaException e) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+		}
+	}
+}
